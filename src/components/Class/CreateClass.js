fda471476e2229d42244009331211bf5fef55a8e
apple <xsherron126@gmail.com>
2024-06-25 23:30:10 +1200

    react

--- src/components/Class/CreateClass.js
+++ src/components/Class/CreateClass.js
@@ -67,7 +67,7 @@ function CreateClass(props) {
             alert("Please assign a lecturer to the class");
             return;
         }
-        axios.post(`${baseUrl}Ass2/classes/`, {
+        axios.post(`${baseUrl}classes/`, {
             number: number,
             course: selectedCourse,
             semester: selectedSemester,
